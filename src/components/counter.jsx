91a91d94e07cd570fa75e676382d2baa4ea3fac1
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-11-07 21:20:10 +0530

    add navbar and some state changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
-  handleIncreament = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button
-          onClick={this.handleIncreament}
+          onClick={() => this.props.onIncreament(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increament
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -31,12 +23,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Counters from "./components/counters";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
